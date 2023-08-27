74f3cd61edf2e93196ae547141ebac708743111e
Rahul Jain <114798084+Rahul-Jain-1@users.noreply.github.com>
2023-08-27 14:13:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,37 +14,6 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-# @app.route("/eegML", methods=["GET", "POST"])
-# def eegML():
-#     if request.method == "POST":
-#         # Get the values from the form
-#         electrode_values = [
-#             float(request.form.get("AF3")),
-#             float(request.form.get("F7")),
-#             float(request.form.get("F3")),
-#             float(request.form.get("FC5")),
-#             float(request.form.get("T7")),
-#             float(request.form.get("P7")),
-#             float(request.form.get("O1")),
-#             float(request.form.get("O2")),
-#             float(request.form.get("P8")),
-#             float(request.form.get("T8")),
-#             float(request.form.get("FC6")),
-#             float(request.form.get("F4")),
-#             float(request.form.get("F8")),
-#             float(request.form.get("AF4"))
-#         ]
-#         electrode_array = np.array([electrode_values])
-#         scaledfeatures = scaler.transform(electrode_array)
-#         prediction1 = classifier.predict(scaledfeatures)
-#         result = ""
-#         if prediction1 == 1:
-#             return render_template("eegML.html",result="Eyes Closed" )
-#         else:
-#             return render_template("eegML.html", result="Eyes Open")
-
-#     return render_template("eegML.html",result="")
-
 app.register_blueprint(eeg_routes)
 @app.route("/chatDataAnalyzer")
 def chatDataAnalyzer():
